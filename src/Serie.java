6967a460b9c39e0c7e18420a3eae40e40049d47c
cph-dr147 <dana.albidani@gmail.com>
2025-11-07 13:07:00 +0100

    Changes by zainabalmosawi

--- src/Serie.java
+++ src/Serie.java
@@ -1,2 +1,12 @@
 public class Serie {
+    int season;
+    int episode;
+
+    public int getSeason() {
+        return season;
+    }
+
+    public int getEpisode() {
+        return episode;
+    }
 }
