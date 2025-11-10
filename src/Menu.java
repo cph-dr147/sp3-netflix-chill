993b57359bd0fcf1405f6a2e9467b705b511e3b6
cph-dr147 <dana.albidani@gmail.com>
2025-11-10 15:03:30 +0100

    movie data

--- src/Menu.java
+++ src/Menu.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+//no need to extend Media
 public class Menu extends Media {
     Scanner scan = new Scanner(System.in);
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,15 +1,29 @@
 import java.time.Year;
 
-public class Movie  extends Media{
+public class Movie  extends Media {
     int year;
+
     public Movie(String title, int year, String genre, double rating) {
         super(title, rating, genre);
         this.year = year;
     }
+
     public void printInfo() {
         System.out.println(title + " (" + year + ") - " + genre + " - Rating: " + rating);
     }
+
     public void playMovie() {
         System.out.println(getTitle() + "Movie is playing...");
+    }
+
+    public Movie(String line) {
+
+        String[] parts = line.split(";");
+        String title = parts[0].trim();
+        int year = Integer.parseInt(parts[1].trim());
+        String genre = parts[2].trim();
+        double rating = Double.parseDouble(parts[3].trim().replace(",", "."));
+
+
     }
 }
